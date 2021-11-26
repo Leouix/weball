bf0fff814b034d63dbd1cc4a580c9fc73058a1d4
Leo <l.starikov@orlimedigital.ru>
2021-11-26 18:02:15 +0300

    init commit

---
+++ neve-child/front-page.php
@@ -0,0 +1,255 @@
+<?php
+
+/*
+ */
+
+get_header();
+
+?>
+
+    <section class="first-view">
+        <div class="first-view-ch first-view-bg"></div>
+        <div class="first-view-ch first-view-content">
+            <marquee behavior="scroll" direction="left" class="first-view-content__el">Бегущая строка</marquee>
+            <h4 class="first-view-content__el first-view-content__hello">Привет, я Leouix!</h4>
+            <h1 class="first-view-content__el first-view-content__title">Я веб-мастер Wordpress</h1>
+            <p class="first-view-content__el first-view-content__desc first-view-content__desc1">Научим делать сайты и работать на удаленке. </p>
+
+            <div><a id="first-i-am-in-button" class="first-view-content__el first-view-content__button">Я в деле!</a></div>
+        </div>
+    </section>
+
+    <section class="regular-section-col second-view">
+        <p class="first-view-content__el first-view-content__desc first-view-content__desc1">Сайты на разработку будут поступать в основном уже существующие, где надо что-то поменять или поправить. </p>
+        <img src="/web-allianse/wp-content/themes/neve-child/images/sky.jpg" alt="">
+        <p class="first-view-content__el first-view-content__desc first-view-content__desc2">Возможно нужно будет что-нибудь доработать, в пределах вашей компетенции. </p>
+        <p class="first-view-content__el first-view-content__desc first-view-content__desc3"><span>Изучив эти материалы, вы сможете следующее:</span> верстка, стили CSS, дизайн, несложные скрипты на jQuery, работа с Wordpress и плагинами. </p>
+    </section>
+
+    <section class="about-me">
+
+        <div class="about-me-inner">
+            <div class="about-me-content">
+                <h4><strong>Section Overline</strong></h4>
+                <h2>About Me</h2>
+                <p class="section-content__desc1">Меня зовут Лев, мой рабочий псевдоним Leouix. Занимаюсь веб-разработкой лет 10 или типа того. По образованию я юрист, но по профессии работать мне не понравилось, стал заниматься тем, к чему больше душа лежала. </p>
+                <p class="section-content__desc1">
+                    Так я сделал свой первый сайт и стал интересоваться его устройством. Сейчас я работаю в международной компании бэкендером, в группе с другими разработчиками с разных стран. </p>
+                <p class="section-content__desc1">Удаленно я стал работать около 5-ти лет назад, но перед самой пандемией специально устроился в офис в студию веб-разработки в Санкт-Петербурге. Сейчас, в связи с ковидом, мне снова пришлось перейти на удаленный режим работы.</p>
+                <p class="section-content__desc1">
+                    На этом месте я работаю где-то полгода. И я очень часто менял место работы. Сначала я работал верстальщиком, потом фронтендером, потом на фуллстак, и сейчас перешел в бэкенд.</p>
+                <p class="section-content__desc1">
+                    Поэтому про то как быстро найти работу, у меня есть специальные навыки. Как пройти собеседование, какие вакансии подойдут под определенный уровень квалификации.
+                </p>
+            </div>
+            <div class="about-me-image"></div>
+        </div>
+
+    </section>
+
+    <section id="plan-education" class="regular-section-col plan-education">
+        <h2>Какой план обучения?</h2>
+        <div class="plan-education-item plan-education-item-1">
+            <div class="plan-education-item__header">
+                <img src="/web-allianse/wp-content/themes/neve-child/images/introductory-icon.png" alt="Как научиться делать сайты">
+                <div class="plan-education-item__header-text">
+                    <div class="pseudo-ol">1. Вводный урок</div>
+                    <ul>
+                        <li>Хостинг, настройка доступа, FTP</li>
+                        <li>Localhost</li>
+                        <li>Домены</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="plan-education-item__video"></div>
+        </div>
+        <div class="plan-education-item plan-education-item-2">
+            <div class="plan-education-item__header">
+                <img class="wp-image" src="/web-allianse/wp-content/themes/neve-child/images/wp-logo.png" alt="Научиться делать сайты с нуля">
+                <div class="plan-education-item__header-text">
+                    <div class="pseudo-ol">2. Wordpress - часть 1</div>
+                    <ul>
+                        <li>Создание сайта</li>
+                        <li>Установка Wordpress</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="plan-education-item__video"></div>
+        </div>
+        <div class="plan-education-item plan-education-item-3">
+            <div class="plan-education-item__header">
+                <img class="wp-image" src="/web-allianse/wp-content/themes/neve-child/images/wp-logo.png" alt="Научиться делать сайт самому">
+                <div class="plan-education-item__header-text">
+                    <div class="pseudo-ol">3. Wordpress - часть 2</div>
+                    <ul>
+                        <li>Изучение админки</li>
+                        <li>Как определить текущий файл сайта</li>
+                        <li>Как создавать дочерние темы</li>
+                        <li>Дебаг-режим</li>
+                        <li>Основа для разработки</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="plan-education-item__video"></div>
+        </div>
+        <div class="compact-plan-continue">
+            <div class="compact-plan-continue__item html-css-item">
+                <img src="/web-allianse/wp-content/themes/neve-child/images/html-css-icon.png" alt="Как самой научиться делать сайт">
+                <div class="plan-education-item__header-text">
+                    <div class="pseudo-ol">4. HTML, CSS - 1 неделя на изучение</div>
+                    <ul>
+                        <li>Основа для разработки</li>
+                        <li>- 1 задание</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="compact-plan-continue__item jquery-item">
+                <img src="/web-allianse/wp-content/themes/neve-child/images/jQuery-logo.png" alt="Как самой делать сайт">
+                <div class="plan-education-item__header-text">
+                    <div class="pseudo-ol">5. jQuery - 3 дня на изучение</div>
+                    <ul>
+                        <li>Самый простой способ к магии программирования. Напишем скрипты, научимся взаимодействовать с элементами, передадим данные из формы на сервер, напишем мобильное меню</li>
+                        <li>- 3 задания</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="compact-plan-continue__item php-item">
+                <img class="php-image" src="/web-allianse/wp-content/themes/neve-child/images/php-logo.png" alt="Как делать сайты">
+                <div class="plan-education-item__header-text">
+                    <div class="pseudo-ol">6. PHP - 3 дня на знакомство</div>
+                    <ul>
+                        <li>Обзор языка программирования PHP, принцип работы, чтение синтаксиса, принцип работы</li>
+                        <li>- 1 задание</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="last-part-education">
+        <div class="river-item-wrapper">
+            <h3>И за что нам, собственно, платят деньги:</h3>
+            <div class="river-item">
+                <div class="plan-education-item__header-text">
+                    <div class="pseudo-ol">7. Wordpress - часть 3 - 1 неделя на изучение</div>
+                    <ul>
+                        <li>Как натянуть верстку на WP</li>
+                        <li>Page templates</li>
+                        <li>Advanced Custom Fields</li>
+                        <li>Custom Post Types</li>
+                        <li>Ajax-php для катосмных форм</li>
+                        <li>Add_action, add_filter</li>
+                        <li>Плагин Duplicator</li>
+                        <li>Обзор Cron'a</li>
+                        <li>$_SERVER, phpinfo()</li>
+                        <li>- 3 задания</li>
+                    </ul>
+                </div>
+                <img src="/web-allianse/wp-content/themes/neve-child/images/river.png" alt="Сайт делаем сами">
+            </div>
+        </div>
+
+        <div id="prices" class="prices">
+            <div class="price-item price-item-1">
+                <div class="tarif-word">Тариф</div>
+                <h3>Решительный</h3>
+                <div class="price">12500 руб.</div>
+                <div class="price-item__description">
+                    <ul>
+                        <li>Обучение</li>
+                        <li>Примеры работ для портфолио</li>
+                        <li>2 консультации (по желанию)</li>
+                    </ul>
+                </div>
+                <div class="price-item__button">Купить</div>
+            </div>
+
+            <div class="price-item price-item-2">
+                <div class="tarif-word">Тариф</div>
+                <h3>Несокрушимый</h3>
+                <div class="price">19500 руб.</div>
+                <div class="price-item__description">
+                    <ul>
+                        <li>Обучение</li>
+                        <li>Примеры работ для портфолио</li>
+                        <li>3 консультации</li>
+                        <li>Проверка заданий</li>
+                        <li>Чат</li>
+                    </ul>
+                </div>
+                <div class="price-item__button">Купить</div>
+            </div>
+
+            <div class="price-item price-item-3">
+                <div class="tarif-word">Тариф</div>
+                <h3>Неудержимый</h3>
+                <div class="price">65000 руб.</div>
+                <div class="price-item__description">
+                    <ul>
+                        <li>Обучение</li>
+                        <li>Примеры работ для портфолио</li>
+                        <li>3 консультации</li>
+                        <li>Проверка заданий</li>
+                        <li>Чат</li>
+
+                        <li class="vip-lis">Помощь в составлении резюме</li>
+                        <li>Сопровождение в подборе работы</li>
+                        <li>Настройка рабочего окружения</li>
+                    </ul>
+                </div>
+                <div class="price-item__button">Купить</div>
+            </div>
+        </div>
+    </section>
+
+    <section class="regular-section-col after-plan">
+        <h2><strong>Всего будет:</strong></h2>
+        <ul>
+            <li>3 недели обучения</li>
+            <li>8 заданий;</li>
+            <li>3 консультации;</li>
+            <li>Составление резюме, согласно полученным знаниям;</li>
+            <li>Подбор подходящих вакансий;</li>
+            <li>Подготовка среды разработки для работы.</li>
+        </ul>
+    </section>
+
+    <section class="regular-section expirience-meet">
+        <div class="expirience-meet-content">
+            <h2>Что нужно чтобы обучиться и работать:</h2>
+            <ul>
+                <li>Компьютер/ноутбук любой мощности;</li>
+                <li>Нормальный интернет.</li>
+                <li>500 рублей на покупку себе хостинга и домена.</li>
+            </ul>
+            <a id="i-m-in-button" class="button" href="">Я в деле</a>
+        </div>
+        <div class="expirience-meet-image"></div>
+    </section>
+
+    <script>
+
+        var imInButton = document.getElementById("i-m-in-button");
+        imInButton.addEventListener('click', function(e){
+            e.preventDefault();
+            scrollToPrices();
+        })
+
+        var imInButtonTop = document.getElementById("first-i-am-in-button");
+        imInButtonTop.addEventListener('click', function(e){
+            e.preventDefault();
+            scrollToPrices();
+        })
+
+        function scrollToPrices(){
+            var scrollDiv = document.getElementById("prices").offsetTop - 100;
+            // var heightDiv = document.getElementById("plan-education").offsetHeight;
+            // var whereToScroll = parseInt(scrollDiv) + parseInt(heightDiv) -850;
+            window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+        }
+
+    </script>
+
+<?php
+get_footer();
---
+++ neve-child/offer-page.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * Template Name: offer-page
+ */
+
+get_header();
+
+?>
+
+<section class="first-view">
+    <div class="first-view-ch first-view-bg"></div>
+    <div class="first-view-ch first-view-content">
+        <marquee behavior="scroll" direction="left" class="first-view-content__el">Бегущая строка</marquee>
+        <h4 class="first-view-content__el first-view-content__hello">Привет, я Leouix!</h4>
+        <h1 class="first-view-content__el first-view-content__title">Я веб-мастер Wordpress</h1>
+        <p class="first-view-content__el first-view-content__desc first-view-content__desc1">Научим делать сайты и работать на удаленке. </p>
+
+        <div><a class="first-view-content__el first-view-content__button">Я в деле!</a></div>
+    </div>
+</section>
+
+<section class="regular-section-col second-view">
+    <p class="first-view-content__el first-view-content__desc first-view-content__desc1">Сайты на разработку будут поступать в основном уже существующие, где надо что-то поменять или поправить. </p>
+    <img src="/web-allianse/wp-content/themes/neve-child/images/sky.jpg" alt="">
+    <p class="first-view-content__el first-view-content__desc first-view-content__desc2">Возможно нужно будет что-нибудь доработать, в пределах вашей компетенции. </p>
+    <p class="first-view-content__el first-view-content__desc first-view-content__desc3"><span>После этого курса это:</span> верстка, стили CSS, дизайн, несложные скрипты на jQuery, работа с Wordpress и плагинами. </p>
+</section>
+
+<section class="regular-section about-me">
+    <div class="about-me-content">
+        <h4><strong>Section Overline</strong></h4>
+        <h2>About Me</h2>
+        <p class="section-content__desc1">Меня зовут Лев, мой рабочий псевдоним Leouix. Занимаюсь веб-разработкой лет 10 или типа того. По образованию я юрист, но по профессии работать мне не понравилось, стал заниматься тем, к чему больше душа лежала. </p>
+        <p class="section-content__desc1">
+            Так я сделал свой первый сайт и стал интересоваться его устройством. Сейчас я работаю в международной компании бэкендером, в группе с другими разработчиками с разных стран. </p>
+        <p class="section-content__desc1">Удаленно я стал работать около 5-ти лет назад, но перед самой пандемией специально устроился в офис в студию веб-разработки в Санкт-Петербурге. Сейчас, в связи с ковидом, мне снова пришлось перейти на удаленный режим работы.</p>
+        <p class="section-content__desc1">
+            На этом месте я работаю где-то полгода. И я очень часто менял место работы. Сначала я работал верстальщиком, потом фронтендером, потом на фуллстак, и сейчас перешел в бэкенд.</p>
+        <p class="section-content__desc1">
+            Поэтому про то как быстро найти работу, у меня есть специальные навыки. Как пройти собеседование, какие вакансии подойдут под определенный уровень квалификации.
+        </p>
+    </div>
+    <div class="about-me-image"></div>
+</section>
+
+<section class="regular-section-col plan-education">
+
+</section>
+
+<section class="regular-section-col after-plan">
+    <h2><strong>Всего будет:</strong></h2>
+    <ul>
+        <li>3 недели обучения</li>
+        <li>8 заданий;</li>
+        <li>3 консультации;</li>
+        <li>Составление резюме, согласно полученным знаниям;</li>
+        <li>Подбор подходящих вакансий;</li>
+        <li>Подготовка среды разработки для работы.</li>
+    </ul>
+</section>
+
+
+<section class="regular-section expirience-meet">
+    <div class="expirience-meet-content">
+        <h2>Что нужно чтобы обучиться и работать:</h2>
+        <ul>
+            <li>Компьютер/ноутбук любой мощности;</li>
+            <li>Нормальный интернет.</li>
+            <li>500 рублей на покупку себе хостинга и домена.</li>
+        </ul>
+        <a class="button" href="">Я в деле</a>
+    </div>
+    <div class="expirience-meet-image"></div>
+</section>
+
+<?php
+get_footer();
